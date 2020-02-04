67ac01d07b8cd39827b8a9b227987a9633020e0b
Shitong Lyu <lyu_s@Shitongs-MacBook.local>
2020-02-04 13:45:25 -0800

    Changed Week 4 Exercise

--- chapter-11-exercises/exercise-1/exercise.R
+++ chapter-11-exercises/exercise-1/exercise.R
@@ -12,7 +12,7 @@ library(fueleconomy)
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
 
-
+# can't get it installed 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
 
